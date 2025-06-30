0603fe8a3c4cde3926b132a6c5290baddf0ab519
binglin <binglin0610@gmail.com>
2025-06-30 21:49:50 +0800

    outstation

---
+++ client/src/units/InWorkstation/coms/AnnualAssessment/index.tsx
@@ -0,0 +1,11 @@
+
+export default defineComponent({
+    name: 'AnnualAssessment',
+    setup() {
+        return () => (
+            <div>
+                <h1>Annual Assessment</h1>
+            </div>
+        )
+    }
+})
