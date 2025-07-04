f7e763786cbf4cc31906e7524c92821a5b8d4e61
qianye <1577121543@qq.com>
2025-07-04 15:40:07 +0800

    前后端联调

---
+++ client/src/api/postdoctor/userinfoRegister/bs_user_profile.ts
@@ -0,0 +1,27 @@
+import fetch_postdoctor from '@/api/postdoctor';
+
+const { raw } = fetch_postdoctor('/api');
+
+// 获取当前用户个人信息
+export const getUserProfile = async () => {
+    const res = await raw.GET('/info/me');
+    return res.data;
+};
+
+// 新增或更新（upsert）个人信息
+export const submitUserProfile = async (data: any) => {
+    const res = await raw.POST('/info/submit', { body: data });
+    return res.data;
+};
+
+// 修改当前用户个人信息
+export const updateUserProfile = async (data: any) => {
+    const res = await raw.PUT('/info/me', { body: data });
+    return res.data;
+};
+
+// 删除当前用户个人信息
+export const deleteUserProfile = async () => {
+    const res = await raw.DELETE('/info/me');
+    return res.data;
+};
